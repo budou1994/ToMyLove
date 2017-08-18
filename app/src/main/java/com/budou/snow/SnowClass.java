507492455e4be2589912deda3abd05cced934d25
budou1994 <1550990175@qq.com>
2017-08-18 23:26:22 +0800

    first commit

---
+++ app/src/main/java/com/budou/snow/SnowClass.java
@@ -0,0 +1,20 @@
+package com.budou.snow;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by 杨大少 on 2017/8/17.
+ */
+
+public class SnowClass extends View {
+
+
+    public SnowClass(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        if (attrs != null) {
+
+        }
+    }
+}
